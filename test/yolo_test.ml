2f4c593089b2677e99a1f56b7f0fa4352e3c826a
Vladimir N. Silyaev <vsilyaev@gmail.com>
2025-04-19 18:50:11 +0000

    fixup! fixup! Added yolo model

--- test/yolo_test.ml
+++ test/yolo_test.ml
@@ -6,14 +6,6 @@ let getfp p field = !@(p |-> field)
 let to_string t = Ctypes.(coerce (ptr char) string t)
 let print pp t = Format.printf "@[%a@]@.%!" pp t
 
-let yolo () =
-  let model = make Types.model_t in
-  let fname = "models/yolov3-tiny.gguf" in
-  let rc = Functions.model_init (addr model) fname in
-  Functions.model_uninit (addr model);
-  assert (rc = 0);
-  ()
-
 let%expect_test "yolo" =
   let model = make Types.model_t in
   let fname = "models/yolov3-tiny.gguf" in
